36f9160824ce24fa78f37fe7167aab7f567bbf59
lazyorcas <thelazyorcas@gmail.com>
2023-09-26 11:59:23 +0200

    fix mailer host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,17 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.default_url_options = { host: "www.1210.social", port: 3000 }
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    user_name: "oscar.1210.social@gmail.com",
+    password: Rails.application.credentials[:mailer][:password],
+    authentication: "plain",
+    enable_starttls_auto: true,
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
