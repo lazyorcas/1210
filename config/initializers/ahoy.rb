346f15cf73988cb2dc1d971d474ca02f4f4cf487
lazyorcas <thelazyorcas@gmail.com>
2023-09-27 15:35:35 +0200

    only mask ips

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 class Ahoy::Store < Ahoy::DatabaseStore
-  def authenticate(data)
-    # disables automatic linking of visits and users
-  end
 end
 
 Ahoy.mask_ips = true
-Ahoy.cookies = false
