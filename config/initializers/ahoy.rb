8f3eca48bfb7a3d57e5898d7bbe37314b01ea882
lazyorcas <thelazyorcas@gmail.com>
2023-09-27 15:16:35 +0200

    mask ip

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -1,14 +1,10 @@
 # frozen_string_literal: true
 
 class Ahoy::Store < Ahoy::DatabaseStore
+  def authenticate(data)
+    # disables automatic linking of visits and users
+  end
 end
 
-# set to true for JavaScript tracking
-Ahoy.api = false
-
-# set to true for geocoding (and add the geocoder gem to your Gemfile)
-# we recommend configuring local geocoding as well
-# see https://github.com/ankane/ahoy#geocoding
-Ahoy.geocode = false
-
 Ahoy.mask_ips = true
+Ahoy.cookies = false
