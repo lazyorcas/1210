92cd22a4084e3a975b4abba8a45dd00e9732d1ab
lazyorcas <thelazyorcas@gmail.com>
2023-09-29 18:06:17 +0200

    delete boilerplate helpers and tests

--- app/helpers/meetup_attendances_helper.rb
+++
@@ -1,2 +0,0 @@
-module MeetupAttendancesHelper
-end
--- test/controllers/meetup_attendances_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class MeetupAttendancesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
