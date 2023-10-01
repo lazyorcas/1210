a2b9d8517de941120a41b7036b5e58475bbe3a26
lazyorcas <thelazyorcas@gmail.com>
2023-10-01 17:41:36 +0200

    default to meetups

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,32 +8,6 @@ class HomeController < ApplicationController
       return
     end
 
-    if today_meetups.empty?
-      redirect_to(today_meetups_path)
-    else
-      redirect_to(today_plans_path)
-    end
-  end
-
-  private
-
-  def today_plans_path
-    plans_path(date: Time.zone.today)
-  end
-
-  def today_meetups_path
-    meetups_path(date: Time.zone.today)
-  end
-
-  def organized_today_meetups
-    current_user.organized_meetups.where(date: Time.zone.today)
-  end
-
-  def accepted_today_meetups
-    current_user.accepted_meetups.where(date: Time.zone.today)
-  end
-
-  def today_meetups
-    organized_today_meetups + accepted_today_meetups
+    redirect_to(meetups_path(date: Time.zone.today))
   end
 end
