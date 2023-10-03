858ed5162b5ff319f8a3e60e0d26a52ef274890d
lazyorcas <thelazyorcas@gmail.com>
2023-10-03 18:23:32 +0200

    fix friends only concern

--- app/models/concerns/friends_only.rb
+++ app/models/concerns/friends_only.rb
@@ -4,18 +4,18 @@ module FriendsOnly
   extend ActiveSupport::Concern
 
   included do
-    before_commit :limit_invitees_to_organizer_friends, if: -> { invitees.present? }, on: [:create, :update]
-    before_create :set_invitees_to_organizer_friends, if: -> { !invitees.present? }
+    before_commit :limit_invitees_to_main_user_friends, if: -> { invitees.present? }, on: [:create, :update]
+    before_create :set_invitees_to_main_user_friends, if: -> { !invitees.present? }
   end
 
   private
 
-  def limit_invitees_to_organizer_friends
-    self.invitees &= organizer.friends
+  def limit_invitees_to_main_user_friends
+    self.invitees &= main_user.friends
   end
 
-  def set_invitees_to_organizer_friends
-    self.invitees = organizer.friends
+  def set_invitees_to_main_user_friends
+    self.invitees = main_user.friends
   end
 
   def main_user
