a1a9a510cf4033f2e16d8e496f6b23dee254c394
lazyorcas <thelazyorcas@gmail.com>
2023-10-08 21:59:43 +0200

    notify organizer if accepted

--- app/models/meetup/invitation.rb
+++ app/models/meetup/invitation.rb
@@ -2,4 +2,18 @@
 
 class Meetup::Invitation < Invitation
   default_scope { where(inviter_type: "Meetup", invitee_type: "User") }
+
+  after_update :notify_organizer, if: -> { is_accepted }
+
+  private
+
+  def notify_organizer
+    inviter.organizer.push_subscriptions.each do |push_subscription|
+      PushNotificationJob.perform_later(
+        push_subscription: push_subscription,
+        title: inviter.title,
+        body: "#{invitee.name} is joining!",
+      )
+    end
+  end
 end
