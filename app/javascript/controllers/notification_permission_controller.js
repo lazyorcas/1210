1a771b3892caaae0af07f744816a4e4d48a1015e
lazyorcas <thelazyorcas@gmail.com>
2023-10-08 16:28:46 +0200

    fix permission shown

--- app/javascript/controllers/notification_permission_controller.js
+++ app/javascript/controllers/notification_permission_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus";
 // Connects to data-controller="notification-permission"
 export default class extends Controller {
   connect() {
-    if (Notification.permission == "default") {
+    if (Notification.permission != "granted") {
       this.element.classList.remove("standalone:hidden");
     }
   }
