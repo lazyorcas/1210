9493b8c92b153e4af3fc7c017e0ac48f35358d04
lazyorcas <thelazyorcas@gmail.com>
2023-10-16 22:48:52 +0200

    only notify organizer if today or future

--- app/models/meetup/invitation.rb
+++ app/models/meetup/invitation.rb
@@ -3,9 +3,13 @@
 class Meetup::Invitation < Invitation
   default_scope { where(inviter_type: "Meetup", invitee_type: "User") }
 
-  after_update :notify_organizer, if: -> { is_accepted }
+  after_update :notify_organizer, if: -> { is_accepted && meetup.today_or_future? }
 
   private
+
+  def meetup
+    inviter
+  end
 
   def notify_organizer
     inviter.organizer.push_subscriptions.each do |push_subscription|
