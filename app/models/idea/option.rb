fd6733cb499c5d437b821240f0440c3bd5ae3072
lazyorcas <thelazyorcas@gmail.com>
2023-10-25 22:40:40 +0200

    do not auto accept idea option initiator

--- app/models/idea/option.rb
+++ app/models/idea/option.rb
@@ -20,7 +20,6 @@ class Idea::Option < Pollable::Option
     as: :inviter
   has_many :upvoters, through: :upvotes, source: :invitee, source_type: "User"
 
-  after_create :create_accepted_invitation_for_idea_user
   after_create :invite_idea_voters
 
   def idea
@@ -28,14 +27,6 @@ class Idea::Option < Pollable::Option
   end
 
   private
-
-  def create_accepted_invitation_for_idea_user
-    Idea::Option::Invitation.create(
-      inviter: self,
-      invitee: idea.user,
-      is_accepted: true,
-    )
-  end
 
   def invite_idea_voters
     idea.voters.each do |user|
