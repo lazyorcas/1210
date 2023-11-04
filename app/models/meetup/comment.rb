3efffab4d2555a4d786780be4a89a403d86b5329
lazyorcas <thelazyorcas@gmail.com>
2023-11-04 15:58:32 +0100

    fix notification

--- app/models/meetup/comment.rb
+++ app/models/meetup/comment.rb
@@ -3,14 +3,20 @@
 class Meetup::Comment < Comment
   default_scope { where(commentable_type: "Meetup") }
 
-  after_create :notify_meetup_invitees
+  after_create :notify_meetup_organizer_and_invitees
 
   def meetup
     commentable
   end
 
-  def notify_meetup_invitees
-    PushSubscription.where(user_id: meetup.invitee_ids).each do |push_subscription|
+  private
+
+  def notifiees
+    [meetup.organizer] + meetup.invitees - [author]
+  end
+
+  def notify_meetup_organizer_and_invitees
+    PushSubscription.where(user: notifiees).each do |push_subscription|
       PushNotificationJob.perform_later(
         push_subscription: push_subscription,
         title: "[Meetup] #{meetup.title}",
