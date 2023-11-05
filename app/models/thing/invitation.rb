223ebc5c7b107260b321c6fdb4cacf357e019fec
lazyorcas <thelazyorcas@gmail.com>
2023-11-05 22:17:52 +0100

    thing notif

--- app/models/thing/invitation.rb
+++ app/models/thing/invitation.rb
@@ -3,5 +3,19 @@
 class Thing::Invitation < Invitation
   default_scope { where(inviter_type: "Thing", invitee_type: "User") }
 
-  validates_uniqueness_of :inviter_id, scope: :invitee_id
+  after_create :notify_thing_interestees_who_are_friends, if: :is_accepted
+
+  def thing
+    inviter
+  end
+
+  def notify_thing_interestees_who_are_friends
+    PushSubscription.where(user: thing.interestees & invitee.friends).each do |push_subscription|
+      PushNotificationJob.perform_later(
+        push_subscription: push_subscription,
+        title: "[#{thing.type}] #{thing.title}",
+        body: "🤩 #{invitee.name} is also interested!",
+      )
+    end
+  end
 end
