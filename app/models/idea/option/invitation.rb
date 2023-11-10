5870a3463ed32621530e791bacb8fbec08023ed2
lazyorcas <thelazyorcas@gmail.com>
2023-11-10 13:21:51 +0100

    notify on idea option accepted

--- app/models/idea/option/invitation.rb
+++ app/models/idea/option/invitation.rb
@@ -4,6 +4,7 @@ class Idea::Option::Invitation < Invitation
   default_scope { where(inviter_type: "Idea::Option", invitee_type: "User") }
 
   after_create :notify_invitee
+  after_update :notify_idea_organizer_and_voters, if: :is_accepted
 
   def idea_option
     inviter
@@ -19,6 +20,16 @@ class Idea::Option::Invitation < Invitation
         push_subscription: push_subscription,
         title: "[Idea] #{idea.title}",
         body: "😎 New option added. Check it out!",
+      )
+    end
+  end
+
+  def notify_idea_organizer_and_voters
+    PushSubscription.where(user: [idea.organizer] + idea.voters).each do |push_subscription|
+      PushNotificationJob.perform_later(
+        push_subscription: push_subscription,
+        title: "[Idea] #{idea.title}",
+        body: "👍 #{invitee.name} - #{idea_option.title}",
       )
     end
   end
