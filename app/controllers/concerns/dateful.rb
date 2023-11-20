5d47d4b23f7a33f48e4d53ddc5b7d3dd5e61c22a
lazyorcas <thelazyorcas@gmail.com>
2023-11-20 21:24:36 +0100

    fix relative_to_today beginning_of_day

--- app/controllers/concerns/dateful.rb
+++ app/controllers/concerns/dateful.rb
@@ -8,7 +8,7 @@ module Dateful
       "Today"
     elsif date == Time.zone.tomorrow
       "Tomorrow"
-    elsif date >= Time.zone.today && date < 1.week.from_now
+    elsif date >= Time.zone.today && date < 1.week.from_now.beginning_of_day
       date.strftime("%A")
     else
       "#{date.strftime("%a")}, #{date.strftime("%B")} #{date.day.ordinalize}"
