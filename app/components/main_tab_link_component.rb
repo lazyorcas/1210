9ce33ffed5c8b6cbdc80b62634ccdaeeb761dd14
lazyorcas <thelazyorcas@gmail.com>
2023-11-24 15:21:09 +0100

    indicate availabilities if none

--- app/components/main_tab_link_component.rb
+++ app/components/main_tab_link_component.rb
@@ -4,10 +4,11 @@ class MainTabLinkComponent < ViewComponent::Base
   include ActionView::Helpers::UrlHelper
   include HeroiconHelper
 
-  def initialize(path:, icon_name:, indicator_count: nil)
+  def initialize(path:, icon_name:, indicator_count: nil, show_indicator: false)
     super
     @path = path
     @icon_name = icon_name
     @indicator_count = indicator_count
+    @show_indicator = show_indicator
   end
 end
