57469d9e8738e6efb4672eeca9f5b0351f37a702
lazyorcas <thelazyorcas@gmail.com>
2023-12-01 23:01:00 +0100

    rack

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -45,20 +45,22 @@ class Rack::Attack
 
   # Block suspicious requests for '/etc/password' or wordpress specific paths.
   # After 3 blocked requests in 10 minutes, block all requests from that IP for 5 minutes.
-  Rack::Attack.blocklist("fail2ban") do |req|
+  blocklist("fail2ban") do |req|
     # `filter` returns truthy value if request fails, or if it's from a previously banned IP
     # so the request is blocked
     Rack::Attack::Fail2Ban.filter("pentesters-#{req.ip}", maxretry: 3, findtime: 10.minutes, bantime: 5.minutes) do
       # The count for the IP is incremented if the return value is truthy
       CGI.unescape(req.query_string) =~ %r{/etc/passwd} ||
         req.path.include?("/etc/passwd") ||
-        req.path.include?("wp") ||
-        req.path.include?("php") ||
-        req.path.include?("db") ||
-        req.path.include?("sql") ||
-        req.path.include?(".env") ||
-        req.path.include?(".git") ||
-        req.path.include?(".DS_STORE")
+        req.path.include?("wp-admin") ||
+        req.path.include?("wp-login")
     end
+  end
+
+  # Always allow requests from localhost
+  # (blocklist & throttles are skipped)
+  safelist("allow from localhost") do |req|
+    # Requests are allowed if the return value is truthy
+    "127.0.0.1" == req.ip || "::1" == req.ip
   end
 end
