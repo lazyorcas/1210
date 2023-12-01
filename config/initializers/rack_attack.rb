0962c56f1c4319e021af3acb88f38b3f3b31fb53
lazyorcas <thelazyorcas@gmail.com>
2023-12-01 15:22:14 +0100

    block requests if needed

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -37,10 +37,28 @@ class Rack::Attack
   end
 
   exploitable = ["public_hashes"]
-
   throttle("exploitable", limit: 1, period: 1.second) do |req|
     if req.path =~ Regexp.union(exploitable)
       req.ip
+    end
+  end
+
+  # Block suspicious requests for '/etc/password' or wordpress specific paths.
+  # After 3 blocked requests in 10 minutes, block all requests from that IP for 5 minutes.
+  Rack::Attack.blocklist("fail2ban") do |req|
+    # `filter` returns truthy value if request fails, or if it's from a previously banned IP
+    # so the request is blocked
+    Rack::Attack::Fail2Ban.filter("pentesters-#{req.ip}", maxretry: 3, findtime: 10.minutes, bantime: 5.minutes) do
+      # The count for the IP is incremented if the return value is truthy
+      CGI.unescape(req.query_string) =~ %r{/etc/passwd} ||
+        req.path.include?("/etc/passwd") ||
+        req.path.include?("wp") ||
+        req.path.include?("php") ||
+        req.path.include?("db") ||
+        req.path.include?("sql") ||
+        req.path.include?(".env") ||
+        req.path.include?(".git") ||
+        req.path.include?(".DS_STORE")
     end
   end
 end
