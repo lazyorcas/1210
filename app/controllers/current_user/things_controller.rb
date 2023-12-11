4862182c81965306aaec5c9b0673942201304490
lazyorcas <thelazyorcas@gmail.com>
2023-12-11 19:39:04 +0100

    fix access

--- app/controllers/current_user/things_controller.rb
+++ app/controllers/current_user/things_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class CurrentUser::ThingsController < AdminController
+class CurrentUser::ThingsController < SocialNetworkController
   layout :resolve_layout
 
   def index
